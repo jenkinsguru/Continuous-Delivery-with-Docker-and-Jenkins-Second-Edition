fe400492816606fb3c10d29400ffe2de54081219
jenkinsguru <jenkinsguru123@gmail.com>
2025-02-16 14:48:46 +0300

    Added test coverage for Calculator class

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -1,12 +1,23 @@
 package com.leszko.calculator;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
-     private Calculator calculator = new Calculator();
+    private Calculator calculator = new Calculator();
 
-     @Test
-     public void testSum() {
-          assertEquals(5, calculator.sum(2, 3));
-     }
+    @Test
+    public void testSum() {
+        // Given
+        int a = 15;
+        int b = 10;
+        
+        // When
+        int result = calculator.sum(a, b);
+        
+        // Then
+        assertEquals(25, result);
+    }
 }
+
+Continuous-Delivery-with-Docker-and-Jenkins-Second-Edition\Chapter08\sample1\src\test\java\com\leszko\calculator\CalculatorTest.java
